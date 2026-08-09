3416e867336f366f7dc5d8caa2e98f4f66699023
Habib MD Saifullah <xavierjames701@gmail.com>
2026-08-09 06:10:05 +0600

    feat: implement addStudent function to handle student creation with validation

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,26 @@
+// Importing service layers
+import { createStudentIntoDB } from "../services/admin.services";
+
+export const addStudent = async (req, res, next) => {
+  try {
+    const studentData = req.body;
+
+    if (!studentData.email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required to add a student!",
+      });
+    }
+
+    //   Sending the request to service layer
+
+    const result = await createStudentIntoDB(studentData);
+    return res.status(201).json({
+      success: true,
+      message: "Student added successfully!",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
